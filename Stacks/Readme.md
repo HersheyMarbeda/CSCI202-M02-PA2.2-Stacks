7e92566eb3c913ed859090fba9c8620657070704
Hershey Marbeda <marbedahershey@gmail.com>
2024-11-02 20:39:39 -0400

    Initial Commit

---
+++ Stacks/Readme.md
@@ -0,0 +1,9 @@
+# CSCI 202: M02 Programming Assignment 2.2-Stacks
+- Programmer: Hershey Marbeda
+- Instructor: Dr. Carver
+- Course: CSCI 202-Data Structures and Algorithms
+- College: Ivy Tech Community College
+- Date Submitted: []
+
+## Assignment Description
+- [ I will update this later.]
---
+++ Stacks/main.cpp
@@ -0,0 +1,25 @@
+/*
+    Assignment Name: M02 Programming Assignment 2.2-Stacks
+    
+    Programmer: Hershey Marbeda
+    Instructor: Dr. Steven Carver
+    Course: CSCI 202-Data Structures and Algorithms
+    College: Ivy Tech Community College
+    Date: 11.04.2024
+ 
+    Progam Description:
+    Last Date Updated: 11.02.2024
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    // To make it readable to screen
+    cout << endl << endl;
+    return 0;
+}
